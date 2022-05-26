d6d0c6b14079e17aa0c5898ce9a72104bc0e2f6b
oleksandrpoddubnyy <oleksandr.poddu@gmail.com>
2022-05-26 23:53:29 +0200

    application.properties conf

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,10 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-links');
-const navLinks = document.querySelectorAll('.nav-links li');
-
+const navSlide = function () {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
 
 
     hamburger.addEventListener('click', function () {
         this.classList.toggle('is-active');
         nav.classList.toggle('nav-active');
+
     });
+}
+
+navSlide();
